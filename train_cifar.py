086abaec998db07565fbbcf1b83ccf9dc674946c
justinryan333 <justin.ryan333@gmail.com>
2025-04-01 04:36:03 -0400

    Update train_cifar.py

--- train_cifar.py
+++ train_cifar.py
@@ -1,7 +1,4 @@
-'''
-TaICML incremental learning
-Copyright (c) Jathushan Rajasegaran, 2019
-'''
+# 1. Imports
 from __future__ import print_function
 import argparse
 import os
@@ -9,38 +6,61 @@ import shutil
 import time
 import pickle
 import torch
-import pdb
 import numpy as np
-import copy
-import torch
 import sys
 import random
 import collections
+from collections import defaultdict
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 
 from utils import mkdir_p
-from basic_net import *
-# from learner_task_reptile import Learner
-# from learner_task_FOMAML import Learner
-# from learner_task_joint import Learner
+from basic_net import BasicNet1
 from learner_task_itaml import Learner
 import incremental_dataloader as data
 
+# 2. FieldTestDataset Class
+class FieldTestDataset(Dataset):
+    def __init__(self, poisoned_data_path):
+        """
+        Loads poisoned test data from .pth file
+        Expected format: torch.save() of PoisonedCIFAR10 object
+        """
+        poisoned_data = torch.load(poisoned_data_path)
+        self.data = poisoned_data.data  # numpy array [N,32,32,3]
+        self.targets = poisoned_data.targets  # list of labels
+        
+        # Match CIFAR-10 preprocessing
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), 
+                              (0.2023, 0.1994, 0.2010))
+        ])
+        
+    def __len__(self):
+        return len(self.targets)
+    
+    def __getitem__(self, idx):
+        img = Image.fromarray(self.data[idx])  # Convert numpy array to PIL
+        return self.transform(img), self.targets[idx]
+        
+# 3. args Class Configuration
 class args:
-
-    checkpoint = "results/cifar100/meta2_cifar_T10_71"
+    # Existing parameters
+    checkpoint = "results/cifar10poison/meta2_cifar_T10_71"
     savepoint = "models/" + "/".join(checkpoint.split("/")[1:])
-    data_path = "../Datasets/CIFAR100/"
+    data_path = "C:/Users/justi/PycharmProjects/iTAML_ATK_Mk1/poisoned_datasets"
     num_class = 10
     class_per_task = 2
     num_task = 5
-    test_samples_per_class = 100
+    test_samples_per_class = 1000
     dataset = "cifar10poison"
     optimizer = "radam"
-
-    epochs = 5
+    epochs = 70
     lr = 0.01
-    train_batch = 128
-    test_batch = 100
+    train_batch = 256
+    test_batch = 200
     workers = 16
     sess = 0
     schedule = [20,40,60]
@@ -51,147 +71,74 @@ class args:
     mu = 1
     beta = 1.0
     r = 2
+    
+    # New field test parameters
+    enable_field_test = True
+   field_test_path = "poison_datasets/test_poisoned_V2.pth"  # Direct path to your file
 
-state = {key:value for key, value in args.__dict__.items() if not key.startswith('__') and not callable(key)}
-print(state)
+# 4. Field Test Helper Functions
+def run_field_test(model, args):
+    """Evaluates model on separate field test dataset"""
+    field_dataset = FieldTestDataset(args.field_test_path)
+    field_loader = DataLoader(field_dataset, 
+                            batch_size=args.test_batch,
+                            num_workers=args.workers)
+    
+    results = {
+        'class_correct': defaultdict(int),
+        'class_total': defaultdict(int)
+    }
+    
+    model.eval()
+    with torch.no_grad():
+        for images, labels in field_loader:
+            images, labels = images.cuda(), labels.cuda()
+            outputs = model(images)[0][:, :args.num_class]  # Only seen classes
+            preds = outputs.argmax(dim=1)
+            
+            for lbl, pred in zip(labels, preds):
+                results['class_total'][lbl.item()] += 1
+                if lbl == pred:
+                    results['class_correct'][lbl.item()] += 1
+    
+    save_field_results(results, args)
+    return results
 
-use_cuda = torch.cuda.is_available()
-seed = random.randint(1, 10000)
-seed = 7572
-random.seed(seed)
-np.random.seed(seed)
-torch.manual_seed(seed)
-if use_cuda:
-    torch.cuda.manual_seed_all(seed)
-
-def main():
-
-    model = BasicNet1(args, 0).cuda()
-#     model = nn.DataParallel(model).cuda()
-
-    print('  Total params: %.2fM ' % (sum(p.numel() for p in model.parameters())/1000000.0))
-
-
-    if not os.path.isdir(args.checkpoint):
-        mkdir_p(args.checkpoint)
-    if not os.path.isdir(args.savepoint):
-        mkdir_p(args.savepoint)
-    np.save(args.checkpoint + "/seed.npy", seed)
-    try:
-        shutil.copy2('train_cifar.py', args.checkpoint)
-        shutil.copy2('learner_task_itaml.py', args.checkpoint)
-    except:
-        pass
-    inc_dataset = data.IncrementalDataset(
-                        dataset_name=args.dataset,
-                        args = args,
-                        random_order=args.random_classes,
-                        shuffle=True,
-                        seed=1,
-                        batch_size=args.train_batch,
-                        workers=args.workers,
-                        validation_split=args.validation,
-                        increment=args.class_per_task,
-                    )
-
-    start_sess = int(sys.argv[1])
-    memory = None
-
-    for ses in range(start_sess, args.num_task):
-        args.sess=ses
-
-        if(ses==0):
-            torch.save(model.state_dict(), os.path.join(args.savepoint, 'base_model.pth.tar'))
-            mask = {}
-
-        if(start_sess==ses and start_sess!=0):
-            inc_dataset._current_task = ses
-            with open(args.savepoint + "/sample_per_task_testing_"+str(args.sess-1)+".pickle", 'rb') as handle:
-                sample_per_task_testing = pickle.load(handle)
-            inc_dataset.sample_per_task_testing = sample_per_task_testing
-            args.sample_per_task_testing = sample_per_task_testing
-
-
-        if ses>0:
-            path_model=os.path.join(args.savepoint, 'session_'+str(ses-1) + '_model_best.pth.tar')
-            prev_best=torch.load(path_model)
-            model.load_state_dict(prev_best)
-
-            with open(args.savepoint + "/memory_"+str(args.sess-1)+".pickle", 'rb') as handle:
-                memory = pickle.load(handle)
-
-        task_info, train_loader, val_loader, test_loader, for_memory = inc_dataset.new_task(memory)
-        print(task_info)
-        print(inc_dataset.sample_per_task_testing)
-        args.sample_per_task_testing = inc_dataset.sample_per_task_testing
-
-
-
-        main_learner=Learner(model=model,args=args,trainloader=train_loader, testloader=test_loader, use_cuda=use_cuda)
-
-        main_learner.learn()
-        memory = inc_dataset.get_memory(memory, for_memory)
-
-        acc_task = main_learner.meta_test(main_learner.best_model, memory, inc_dataset)
-
-
-        with open(args.savepoint + "/memory_"+str(args.sess)+".pickle", 'wb') as handle:
-            pickle.dump(memory, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-        with open(args.savepoint + "/acc_task_"+str(args.sess)+".pickle", 'wb') as handle:
-            pickle.dump(acc_task, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-        with open(args.savepoint + "/sample_per_task_testing_"+str(args.sess)+".pickle", 'wb') as handle:
-            pickle.dump(args.sample_per_task_testing, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-
+def save_field_results(results, args):
+    """Saves human-readable test results"""
+    os.makedirs(args.savepoint, exist_ok=True)
+    
+    with open(f'{args.savepoint}/field_test_results.txt', 'w') as f:
+        # Header
+        f.write(f"Field Test Results ({time.ctime()})\n")
+        f.write("="*50 + "\n\n")
         
-        print("+++++++++++++++++++++++++++++++++++++++++++++++")
-        print("switching To Poison for Evaluation")
-        args.dataset = "cifar10poison_test"  # Switch to the poisoned test dataset
+        # Overall Accuracy
+        total_correct = sum(results['class_correct'].values())
+        total_samples = sum(results['class_total'].values())
+        f.write(f"OVERALL ACCURACY: {100*total_correct/total_samples:.2f}% "
+               f"({total_correct}/{total_samples})\n\n")
+        
+        # Class-wise Results
+        f.write("CLASS\tACCURACY\tCORRECT/TOTAL\n")
+        f.write("-----\t--------\t-------------\n")
+        for class_id in sorted(results['class_correct'].keys()):
+            acc = 100 * results['class_correct'][class_id] / results['class_total'][class_id]
+            f.write(f"{class_id:5d}\t{acc:8.2f}%\t"
+                   f"{results['class_correct'][class_id]:4d}/{results['class_total'][class_id]:4d}\n")
 
-        # Reinitialize the dataset with the poisoned test dataset
-        inc_dataset = data.IncrementalDataset(
-            dataset_name=args.dataset,
-            args=args,
-            random_order=args.random_classes,
-            shuffle=True,
-            seed=1,
-            batch_size=args.train_batch,
-            workers=args.workers,
-            validation_split=args.validation,
-            increment=args.class_per_task,
-        )
-
-        # Create new loaders for testing (using the poisoned test dataset)
-        _, _, _, test_loader, _ = inc_dataset.new_task(memory)
-
-        # Evaluate on the poisoned test dataset
-        acc_task_test = main_learner.meta_test(main_learner.best_model, memory, inc_dataset)
-
-        # Save the test accuracy
-        with open(args.savepoint + "/acc_task_test_" + str(args.sess) + ".pickle", 'wb') as handle:
-            pickle.dump(acc_task_test, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-        time.sleep(10)
-
-        # Switch back to the original poisoned training dataset for the next session (if any)
-        print("switching back")
-        print("+++++++++++++++++++++++++++++++++++++++++++++++")
-        args.dataset = "cifar10poison"  # Switch back to the original poisoned training dataset
-
-        # Reinitialize the dataset for the next session with the original poisoned training dataset
-        inc_dataset = data.IncrementalDataset(
-            dataset_name=args.dataset,
-            args=args,
-            random_order=args.random_classes,
-            shuffle=True,
-            seed=1,
-            batch_size=args.train_batch,
-            workers=args.workers,
-            validation_split=args.validation,
-            increment=args.class_per_task,
-        )
+# 5. Original Main Function (unchanged)
+def main():
+    # ... (your existing main() implementation) ...
 
 if __name__ == '__main__':
     main()
+    
+    if args.enable_field_test:
+        print("\nRunning field test evaluation on poisoned data...")
+        final_model = BasicNet1(args, 0).cuda()
+        final_model.load_state_dict(
+            torch.load(os.path.join(args.savepoint, f'session_{args.num_task-1}_model_best.pth.tar'))
+        
+        results = run_field_test(final_model, args)
+        print(f"Poisoned field test results saved to {args.savepoint}/field_test_results.txt")
